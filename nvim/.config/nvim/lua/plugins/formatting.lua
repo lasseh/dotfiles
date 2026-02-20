dfaefa5611011267e6cb1d20895880865009c6db
Lasse <haugen.lasse@gmail.com>
2026-02-20 09:16:24 +0100

    feat: add conform.nvim for format-on-save
    Configure goimports for Go and stylua for Lua with format-on-save
    enabled. Adds <leader>cf keybinding for manual formatting.

---
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,23 @@
+return {
+  "stevearc/conform.nvim",
+  event = "BufWritePre",
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      go = { "goimports" },
+      lua = { "stylua" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+  },
+}
